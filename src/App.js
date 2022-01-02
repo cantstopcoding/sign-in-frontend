cd8dd66c0d4d541d10ad464634fd4c79b7f7b793
cantstopcoding <jonathan_koss@yahoo.com>
2022-01-02 16:37:31 -0500

    check to see if fron and back end are communicating

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useEffect, useState } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+  const [books, setBooks] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:3000/api/v1/books")
+      .then((response) => response.json())
+      .then((data) => {
+        setBooks(data);
+        console.log(books);
+      })
+      .catch((error) => {
+        console.log("error!");
+      });
+  }, []);
+  return <div className="App">App</div>;
+};
 
 export default App;
