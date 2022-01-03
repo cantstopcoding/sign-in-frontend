621097c8927dac605dc39d6479610ffdc4e43ec5
cantstopcoding <jonathan_koss@yahoo.com>
2022-01-02 19:03:19 -0500

    keep state of form

--- src/components/signup/index.js
+++ src/components/signup/index.js
@@ -1,36 +1,61 @@
+import { useState } from "react";
 import { FormStyle } from "./styles";
 
 const SignUpForm = () => {
+  const [userInfo, setUserInfo] = useState({});
+
   const signUp = (event) => {
+    event.preventDefault();
     // contact API to signup
+    fetch("");
   };
+
+  const handleInputChange = (event) => {
+    const { name, value } = event.target;
+    setUserInfo({ ...userInfo, [name]: value });
+  };
+
+  // const handleInputChange = (event) => {
+  //   const { name, value } = event.target;
+  //   console.log(" name:: ", name, " value:: ", value);
+  //   setUserInfo({ ...userInfo, [name]: value });
+  // };
 
   return (
     <FormStyle>
       <form onSubmit={signUp}>
+        <h1>Sing Up:</h1>
         <label htmlFor="username">Username</label>
         <br />
-        <input type="text" name="username" placeholder="username" />
+        <input
+          type="text"
+          name="username"
+          placeholder="username"
+          onChange={handleInputChange}
+        />
         <br />
         <label htmlFor="email">Email</label>
         <br />
-        <input type="email" name="email" placeholder="email" />
+        <input
+          type="email"
+          name="email"
+          placeholder="email"
+          onChange={handleInputChange}
+        />
         <br />
         <label htmlFor="password">Password:</label>
         <br />
-        <input type="password" name="password" placeholder="password" />
+        <input
+          type="password"
+          name="password"
+          placeholder="password"
+          onChange={handleInputChange}
+        />
         <br />
-        <input type="submit" />
+        <input type="submit" onChange={handleInputChange} />
       </form>
     </FormStyle>
   );
 };
 
 export default SignUpForm;
-
-//     t.string "name"
-//     t.string "username"
-//     t.string "password_digest"
-//     t.datetime "created_at", precision: 6, null: false
-//     t.datetime "updated_at", precision: 6, null: false
-//     t.string "email"
